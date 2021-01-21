c96c7ed59676d0d5e8857761bf5c5a0a31b591c1
Chaz <be004779@student.reading.ac.uk>
2021-01-21 13:08:49 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/guessinggame/Guess.java
@@ -0,0 +1,18 @@
+package com.example.guessinggame;
+
+public class Guess {
+    private int valueToGuess;
+
+    public Guess(){
+
+    }
+
+    void setNewValue(){
+
+    }
+
+    boolean makeGuess(int a){
+
+        return false;
+    }
+}
